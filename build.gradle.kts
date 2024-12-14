8cd27077f515d4ccc2f729f80d316cac4ccfaaef
Koki4a <112828839+Koki4a08@users.noreply.github.com>
2024-12-14 19:18:05 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,18 +26,20 @@ repositories {
 
     maven("https://papermc.io/repo/repository/maven-public/") // Paper
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") // Spigot
-    maven("https://oss.sonatype.org/content/repositories/snapshots") // Because Spigot depends on Bungeecord ChatComponent-API
+    maven("https://oss.sonatype.org/content/repositories/snapshots") // For Bungeecord ChatComponent-API
     maven("https://jitpack.io") // JitPack
     maven("https://repo.dmulloy2.net/repository/public/") // ProtocolLib
     maven("https://libraries.minecraft.net/") // Minecraft repo (commodore)
-    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/") // PlaceHolderAPI
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/") // PlaceholderAPI
     maven("https://maven.elmakers.com/repository/") // EffectLib
-    maven("https://hub.jeff-media.com/nexus/repository/jeff-media-public/") // CustomBlockData
+    maven("https://hub.jeff-media.com/nexus/repository/jeff-media-public/") // CustomBlockData & MorePersistentDataTypes & PersistentDataSerializer
     maven("https://repo.triumphteam.dev/snapshots") // actions-code, actions-spigot
-    maven("https://mvn.lumine.io/repository/maven-public/") { metadataSources { artifact() } }// MythicMobs
+    maven("https://mvn.lumine.io/repository/maven-public/") {
+        metadataSources { artifact() } 
+    } // MythicMobs
     //maven("https://mvn.lumine.io/repository/maven/") // PlayerAnimator
     maven("https://repo.mineinabyss.com/releases") // PlayerAnimator
-    maven("https://s01.oss.sonatype.org/content/repositories/snapshots") // commandAPI snapshots
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots") // CommandAPI snapshots
     maven("https://maven.enginehub.org/repo/")
 }
 
@@ -61,7 +63,7 @@ dependencies {
     compileOnly("dev.jorel:commandapi-bukkit-shade:$commandApiVersion")
     compileOnly("io.lumine:MythicLib:1.1.6")
     compileOnly("net.Indyuce:MMOItems:6.7.3")
-    compileOnly("org.joml:joml:1.10.5") // Because pre 1.19.4 api does not have this in the server-jar
+    compileOnly("org.joml:joml:1.10.5")
 
     implementation("dev.triumphteam:triumph-gui:3.1.5")
     implementation("org.bstats:bstats-bukkit:3.0.0")
